dad0b8aa2c20ca32175b6f31afdcbbc61a7cb8f8
Clément <86530475+clementciron@users.noreply.github.com>
2022-11-23 18:27:50 +0100

    Update hacker-rank-1.c

--- hacker-rank-1.c
+++ hacker-rank-1.c
@@ -5,35 +5,17 @@
 
 
 
+  
 int main() {
     int n;
     scanf("%d", &n);
-    ft_loop(n);
-    return 0;
-}
+    int sum = 0;
 
-
-void ft_loop(int n){
-    
-    int arr[5];
-    int i = 0;
-    int m = 10;
-    while (i < 4)
-    {
-        if(arr[i] < 10)
-        {
-            arr[i] = n%m;
-            ft_loop(arr[i]);
-        }
-        if(arr[i] >= 10)
-        {
-            arr[i] = n/10;
-            m = m * 10;
-            i++;
-            printf("%d\n", arr[i]);
-        }
+    while(n){
+        sum = sum + n % 10;
+        n /= 10;
     }
-    int r = arr[0] + arr[1] + arr[2] + arr[3] + arr[4];
-    printf("%d", r);
-    
+    printf("%d\n", sum);
+
+    return 0;
 }
