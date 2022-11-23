b0836ccdc37d677a3c41334de64ad9cc57441a95
Clément <86530475+clementciron@users.noreply.github.com>
2022-11-23 18:28:58 +0100

    Update hacker-rank-1.c

--- hacker-rank-1.c
+++ hacker-rank-1.c
@@ -19,3 +19,18 @@ int main() {
 
     return 0;
 }
+
+
+/*
+Objective
+The modulo operator, %, returns the remainder of a division. For example, 4 % 3 = 1 and 12 % 10 = 2. The ordinary division operator, /,
+returns a truncated integer value when performed on integers. For example, 5 / 3 = 1. To get the last digit of a number in base 10, use
+as the modulo divisor.
+
+Task
+Given a five digit integer, print the sum of its digits.
+Input Format
+The input contains a single five digit number,
+Print the sum of the digits of the five digit number.
+
+*/
