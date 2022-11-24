f7befb097803e422490d397684fc9089716389b2
Clément <86530475+clement-crn@users.noreply.github.com>
2022-11-24 16:49:48 +0100

    Update leetcode-twosum.c

--- leetcode-twosum.c
+++ leetcode-twosum.c
@@ -1,27 +1,31 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
 
     int *return_array = malloc(2 * sizeof(int));
     
     int i = 0;
-    int y = 1;
+    int y;
     
-    return_array[0] = 0;
-    return_array[1] = 0;
+    return_array[0] =0;
+    return_array[1] =1;
     
     while(i < numsSize){
+         y = i + 1;
         while(y < numsSize){
+            
             if (nums[i] + nums[y] == target){
                 *returnSize=2;
+                
                 return_array[0] = i;
                 return_array[1] = y;
             }
             y++;
         }
         i++;
-    } 
-return return_array;
+    }
+    y = 2;
+    
+    return return_array;
 }
